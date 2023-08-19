1ef7f3995e9a70a0e7cfd4cf0a09363d00c19a56
Sam Shock <sam.shock924@gmail.com>
2023-08-19 13:26:20 -0400

    starting basic css

--- client/src/components/NavbarContainer.jsx
+++ client/src/components/NavbarContainer.jsx
@@ -2,21 +2,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Container} from 'react-bootstrap';
+import '../component_css/NavbarContainer.css';
 
 const NavbarContainer = () => {
 	return (
-		<Navbar className='bg-body-tertiary'>
+		<Navbar className='navbar'>
 			<Container>
-				<Navbar.Brand href='#home' expand='lg'>
-					<img
-						alt=''
-						src='/spotify-logo-orange.png'
-						width='30'
-						height='30'
-						className='d-inline-block align-top'
-					/>{' '}
+				<Navbar.Brand href='#home' className='navbar-home'>
+					<img alt='' src='/spotify-logo-orange.png' width='45' height='45' />{' '}
 					Home
 				</Navbar.Brand>
+				<Nav>Search</Nav>
 			</Container>
 		</Navbar>
 	);
