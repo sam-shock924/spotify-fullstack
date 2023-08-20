0849a8b3bbded63d06293ad40533d5ab574616ad
Sam Shock <sam.shock924@gmail.com>
2023-08-20 14:20:52 -0400

    basic login w/ pw reenter

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -8,11 +8,11 @@ function LoginForm() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(username, password);
+		// Handle login logic here
 	};
 
 	return (
-		<Form onSubmit={handleSubmit}>
+		<Form className='spotify-form' onSubmit={handleSubmit}>
 			<Form.Group controlId='username'>
 				<Form.Label>Username</Form.Label>
 				<Form.Control
@@ -22,7 +22,6 @@ function LoginForm() {
 					placeholder='Enter username'
 				/>
 			</Form.Group>
-
 			<Form.Group controlId='password'>
 				<Form.Label>Password</Form.Label>
 				<Form.Control
@@ -32,8 +31,21 @@ function LoginForm() {
 					placeholder='Enter password'
 				/>
 			</Form.Group>
+			<Form.Group controlId='password'>
+				<Form.Label>Re-Enter Password</Form.Label>
+				<Form.Control
+					type='password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder='Enter password'
+				/>
+			</Form.Group>
 
-			<Button variant='primary' type='submit'>
+			<Form.Text>
+				Please use a combination of letters, numbers, and special characters
+				such as !?_*#$%
+			</Form.Text>
+			<Button className='spotify-button' type='submit'>
 				Log In
 			</Button>
 		</Form>
@@ -41,3 +53,5 @@ function LoginForm() {
 }
 
 export default LoginForm;
+
+// export default LoginForm;
