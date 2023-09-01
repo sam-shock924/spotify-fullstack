90dbc27008bbd798aaf9de89743f2e82b169b730
Sam Shock <sam.shock924@gmail.com>
2023-09-01 11:47:06 -0400

    working on css updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
-import {useState, useEffect} from 'react';
 import './App.css';
-import axios from 'axios';
 import Navbar from './components/NavbarContainer';
 import Signup from './components/Signup';
 import {
@@ -12,7 +10,6 @@ import {
 import Home from './components/Home';
 import Search from './components/Search';
 import PlaylistPage from './components/PlaylistPage';
-import NavbarContainer from './components/NavbarContainer';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
--- client/src/components/NavbarContainer.jsx
+++ client/src/components/NavbarContainer.jsx
@@ -1,16 +1,25 @@
 import {Outlet, NavLink} from 'react-router-dom';
-// import '../component_css/NavbarContainer.css';
+import '../component_css/NavbarContainer.css';
 
 const NavbarContainer = () => {
 	return (
-		<div>
-			<NavLink to='/' className='navbar-home'>
-				<img alt='' src='/spotify-logo-orange.png' width='45' height='45' />{' '}
-				Home
-			</NavLink>
-			<NavLink to='search'>Search</NavLink>
-			<NavLink to='playlists'>Playlists</NavLink>
-			<NavLink to='user-data'>User placeholder</NavLink>{' '}
+		<div className='navbar-container'>
+			<div className='navbar-home-container'>
+				<NavLink to='/' className='navbar-home'>
+					<img alt='' src='/spotify-logo-orange.png' width='45' height='45' />
+				</NavLink>
+			</div>
+			<div className='navbar-links-container'>
+				<NavLink className='navbar-links' to='search'>
+					Search
+				</NavLink>
+				<NavLink className='navbar-links' to='playlists'>
+					Playlists
+				</NavLink>
+				<NavLink className='navbar-links' to='user-data'>
+					User placeholder
+				</NavLink>
+			</div>
 			<main>
 				<Outlet />
 			</main>
