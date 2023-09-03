67b7eeacba6cf97e8a1a55c37e53dc00179c4b1d
Sam Shock <sam.shock924@gmail.com>
2023-09-03 12:31:22 -0400

    pulled page info off navbar

--- client/src/components/NavbarContainer.jsx
+++ client/src/components/NavbarContainer.jsx
@@ -3,27 +3,29 @@ import '../component_css/NavbarContainer.css';
 
 const NavbarContainer = () => {
 	return (
-		<div className='navbar-container'>
-			<div className='navbar-home-container'>
-				<NavLink to='/' className='navbar-home'>
-					<img alt='' src='/spotify-logo-orange.png' width='45' height='45' />
-				</NavLink>
-			</div>
-			<div className='navbar-links-container'>
-				<NavLink className='navbar-links' to='search'>
-					Search
-				</NavLink>
-				<NavLink className='navbar-links' to='playlists'>
-					Playlists
-				</NavLink>
-				<NavLink className='navbar-links' to='user-data'>
-					User placeholder
-				</NavLink>
+		<>
+			<div className='navbar-container'>
+				<div className='navbar-home-container'>
+					<NavLink to='/' className='navbar-home'>
+						<img alt='' src='/spotify-logo-orange.png' width='45' height='45' />
+					</NavLink>
+				</div>
+				<div className='navbar-links-container'>
+					<NavLink className='navbar-links' to='search'>
+						Search
+					</NavLink>
+					<NavLink className='navbar-links' to='playlists'>
+						Playlists
+					</NavLink>
+					<NavLink className='navbar-links' to='user-data'>
+						User placeholder
+					</NavLink>
+				</div>
 			</div>
 			<main>
 				<Outlet />
 			</main>
-		</div>
+		</>
 	);
 };
 
