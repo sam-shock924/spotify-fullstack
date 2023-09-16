da752c8b1c950007fcb2a7115e26a3881f7f5abc
Sam Shock <sam.shock924@gmail.com>
2023-09-16 12:58:42 -0400

    updated home buttons to links

--- client/src/components/HomeLoggedOut.jsx
+++ client/src/components/HomeLoggedOut.jsx
@@ -1,6 +1,19 @@
 import {Button} from 'react-bootstrap';
+import {NavLink} from 'react-router-dom';
 import '../component_css/HomeLoggedOut.css';
 
+// export default function HomeLoggedOut() {
+// 	return (
+// 		<div className='home-logged-out-container'>
+// 			<div className='home-logged-out-title-container'>
+// 				<h3 className='home-logged-out-title1'>Welcome to</h3>
+// 				<h1 className='home-logged-out-title2'>Your Spotify!</h1>
+// 			</div>
+// 			<Button className='home-logged-out-button'>Sign Up</Button>
+// 			<Button className='home-logged-out-button'>Log In</Button>
+// 		</div>
+// 	);
+// }
 export default function HomeLoggedOut() {
 	return (
 		<div className='home-logged-out-container'>
@@ -8,8 +21,10 @@ export default function HomeLoggedOut() {
 				<h3 className='home-logged-out-title1'>Welcome to</h3>
 				<h1 className='home-logged-out-title2'>Your Spotify!</h1>
 			</div>
-			<Button className='home-logged-out-button'>Sign Up</Button>
-			<Button className='home-logged-out-button'>Log In</Button>
+			<NavLink to='signup' className='home-logged-out-button'>
+				Sign Up
+			</NavLink>
+			<NavLink className='home-logged-out-button'>Log In</NavLink>
 		</div>
 	);
 }
