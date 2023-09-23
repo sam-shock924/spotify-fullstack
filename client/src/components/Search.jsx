9bbaccfb5929e6ec0649205c94988101f1e8376d
Sam Shock <sam.shock924@gmail.com>
2023-09-23 11:52:49 -0400

    added basic search

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,3 +1,30 @@
 export default function Search() {
-	return <div>Search</div>;
+	return (
+		<div>
+			<h1>Search Page</h1>
+			<form className='search-input'>
+				<input
+					id='search-value'
+					type='text'
+					// value={query}
+					// onChange={handleQuery}
+					placeholder='Search...'
+				/>
+				<select
+					// value={queryType}
+					// onChange={handleQueryType}
+					name='default'
+					id='select-options'
+				>
+					Search for...
+					<option value='placeholder'>Search for...</option>
+					<option value='artist'>Artist</option>
+					<option value='album'>Album</option>
+					<option value='track'>Song</option>
+				</select>
+				<button type='submit'>Search</button>
+			</form>
+			{/* <Output queryList={queryList} /> */}
+		</div>
+	);
 }
